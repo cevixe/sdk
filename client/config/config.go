721eadee4a2c2153d8ecf5a9f6f7a65acede2ae2
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-11-21 17:51:48 -0600

    add warmup

--- client/config/config.go
+++ client/config/config.go
@@ -13,13 +13,16 @@ import (
 func NewConfig(ctx context.Context) aws.Config {
 
 	region := os.Getenv("AWS_REGION")
+	httpClient := http.NewDefaultClient()
+	http.WarmUp(httpClient, region, []string{"dynamodb", "sns", "s3"})
 	cfg, err := config.LoadDefaultConfig(ctx,
 		config.WithDefaultRegion(region),
-		config.WithHTTPClient(http.NewDefaultClient()),
+		config.WithHTTPClient(httpClient),
 	)
 
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
+
 	return cfg
 }
