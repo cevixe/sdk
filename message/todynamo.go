bce1742ca2e40dd23d03b4229819f6ae6fb9854a
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-11-21 23:33:29 -0600

    fix message marshal to dynamodb

--- message/todynamo.go
+++ message/todynamo.go
@@ -1,6 +1,8 @@
 package message
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/pkg/errors"
@@ -8,7 +10,18 @@ import (
 
 func ToDynamodb_Map(msg Message) (map[string]types.AttributeValue, error) {
 	impl := msg.(*messageImpl)
-	item, err := attributevalue.MarshalMap(impl)
+
+	buffer, err := json.Marshal(impl)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot marshal message object to json")
+	}
+
+	msgMap := make(map[string]interface{})
+	if err = json.Unmarshal(buffer, &msgMap); err != nil {
+		return nil, errors.Wrap(err, "cannot unmarshal json message to map")
+	}
+
+	item, err := attributevalue.MarshalMap(msgMap)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot generate dynamo map from entity data")
 	}
